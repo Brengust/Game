e7e5206900dee1928c65e391bd29b08eb2be6249
Aaron <amccommon349@email.arizona.edu>
2019-05-21 22:05:23 -0700

    Dab

--- Main.py
+++ Main.py
@@ -1,12 +1,18 @@
 import pyglet
+from pyglet.window import key, mouse
 
 window = pyglet.window.Window()
-label = pyglet.text.Label('Hello, world', font_name = 'Times New Roman', font_size = 36, x = window.width//2, y = window.height//2,
-                          anchor_x = 'center', anchor_y = 'center')
+window.push_handlers(pyglet.window.event.WindowEventLogger())
+
+music = pyglet.resource.media("Cros'.wav")
+music.play()
+
+@window.event
+def on_key_press(symbol, modifiers):
+    print('A key was pressed')
 
 @window.event
 def on_draw():
     window.clear()
-    label.draw()
 
 pyglet.app.run()
